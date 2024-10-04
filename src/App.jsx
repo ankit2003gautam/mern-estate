f6f59735386bd2d851937a0f0d22598ca719a71c
Ankit gautam <gautamankiflag2003@gmail.com>
2024-10-04 23:35:53 +0530

    install react js and tailwind css and create the first template

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function App() {
   return (
-    <h1 className="text-green-500">Class proves the app App</h1>
+    <h1 className="text-orange-500">Class proves the app App</h1>
   );
 }
